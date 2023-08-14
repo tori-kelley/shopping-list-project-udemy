e60bd66f74fada4bacd547faee5cc63967bf7344
Tori <torikelley2023@gmail.com>
2023-08-14 12:52:25 -0700

    remove and clear items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearButton = document.getElementById("clear");
 
 function addItem(e) {
     e.preventDefault();
@@ -39,5 +40,19 @@ function createIcon(classes) {
     return icon;
 }
 
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains("remove-item")) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+function clearItems(e) {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 //event listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearButton.addEventListener("click",clearItems)
